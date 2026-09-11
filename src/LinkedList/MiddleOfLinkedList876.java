4cb4fba856b96b894908fd930df72eea9ca13a1b
aniket-d18 <aniketchaudhari3984@gmail.com>
2026-09-11 22:51:14 +0530

    Solved Linked List

--- src/LinkedList/MiddleOfLinkedList876.java
+++ src/LinkedList/MiddleOfLinkedList876.java
@@ -2,21 +2,35 @@ package LinkedList;
 
 class ListNode876 {
     int val;
-    ListNode next;
+    ListNode876 next;
     ListNode876() {}
     ListNode876(int val) { this.val = val; }
-    ListNode876(int val, ListNode next) { this.val = val; this.next = next; }
-    }
-class SolutionofLinkedList {
-    public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+    ListNode876(int val, ListNode876 next) { this.val = val; this.next = next; }
+}
 
+class SolutionofLinkedList {
+    public ListNode876 middleNode(ListNode876 head) {
+        ListNode876 slow = head;
+        ListNode876 fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
         return slow;
     }
 }
-public class MiddleOfLinkedList876 {
-    static void main() {
 
+public class MiddleOfLinkedList876 {
+    public static void main(String[] args) {
+        ListNode876 head = new ListNode876(1,
+                new ListNode876(2,
+                        new ListNode876(3,
+                                new ListNode876(4,
+                                        new ListNode876(5)))));
+
+        SolutionofLinkedList sol = new SolutionofLinkedList();
+        ListNode876 mid = sol.middleNode(head);
+        System.out.println(mid.val); // Output: 3
     }
 }
